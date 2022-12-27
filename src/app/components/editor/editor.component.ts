57b924e83d9b107ddbc6323f1195234f96d7b27b
Albert Panesse <albert.panesse@gmail.com>
2022-12-27 16:34:50 +0700

    ok banged

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,42 +1,71 @@
-import { AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from "@angular/core";
+
+declare var window: any;
 
 @Component({
   selector: 'editor-comp',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class NEditorComponent implements OnChanges, AfterViewInit {
+export class NEditorComponent implements OnInit, OnChanges, AfterViewInit {
   @Input() textToAdd: string = '';
+  @ViewChild('editorContainer') editorContainer: ElementRef;
+
+  private editor: any = null;
+  private model: any = null;
 
   public options = {
     language: 'sql',
   };
   public text: string = '';
 
+  constructor() {}
+
+  async ngOnInit() {
+    await Promise.all([
+      new Promise<void>((resolve) => {
+        const loaderScript: HTMLScriptElement = document.createElement('script');
+        loaderScript.type = 'text/javascript';
+        loaderScript.src = '/assets/monaco-editor/vs/loader.js';
+        document.body.appendChild(loaderScript);
+        loaderScript.addEventListener('load', () => resolve());
+      }),
+      new Promise<void>((resolve) => {
+        const editorNlsScript: HTMLScriptElement = document.createElement('script');
+        editorNlsScript.type = 'text/javascript';
+        editorNlsScript.src = '/assets/monaco-editor/vs/editor/editor.main.nls.js'
+        document.body.appendChild(editorNlsScript);
+        editorNlsScript.addEventListener('load', () => resolve());
+      }),
+      new Promise<void>((resolve) => {
+        const editorScript: HTMLScriptElement = document.createElement('script');
+        editorScript.type = 'text/javascript';
+        editorScript.src = '/assets/monaco-editor/vs/editor/editor.main.js'
+        document.body.appendChild(editorScript);
+        editorScript.addEventListener('load', () => resolve());
+      }),
+    ]);
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     const _textToAdd = changes['textToAdd'];    
-    this.text = `${this.text}${_textToAdd.currentValue}`;
+    // this.text = `${this.text}${_textToAdd.currentValue}`;
+    if (this.editor) {
+      const model = this.editor.getModel();
+      model.pushEditOperations(
+        [],
+        [{ range: model.getFullModelRange(), text: _textToAdd.currentValue }],
+        () => null,
+    );
+    }
   }
 
-  ngAfterViewInit(): void {
-    const loaderScript: HTMLScriptElement = document.createElement('script');
-    loaderScript.type = 'text/javascript';
-    loaderScript.src = '/assets/monaco-editor/vs/loader.js';
-
-    const editorNlsScript: HTMLScriptElement = document.createElement('script');
-    editorNlsScript.type = 'text/javascript';
-    editorNlsScript.src = '/assets/monaco-editor/vs/editor/editor.main.nls.js'
-
-    const editorScript: HTMLScriptElement = document.createElement('script');
-    editorScript.type = 'text/javascript';
-    editorScript.src = '/assets/monaco-editor/vs/editor/editor.main.js'
-    
-    document.body.appendChild(loaderScript);
-    document.body.appendChild(editorNlsScript);
-    document.body.appendChild(editorScript);
-  }
-
-  onGotAmdLoader($event: any) {
-    console.log('$event', $event);
+  ngAfterViewInit() {
+    const interval = setInterval(() => {
+      if (window.monaco) {        
+        this.editor = window.monaco.editor.create(this.editorContainer.nativeElement);
+        clearInterval(interval);
+      }
+    }, 100);
   }
 }
